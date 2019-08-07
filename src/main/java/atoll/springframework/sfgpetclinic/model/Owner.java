160810f041852ec45067a969a9e2afa25cc694d4
Kazrath <f.balint5.3@gmail.com>
2019-08-07 12:15:13 +0200

    Adding POJOs. Closes #2

---
+++ src/main/java/atoll/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package atoll.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/atoll/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package atoll.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
