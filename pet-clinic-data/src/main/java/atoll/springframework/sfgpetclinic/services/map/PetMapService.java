b3250486a10f00bedfd9cdbcdc020bcdefed5916
Kazrath <f.balint5.3@gmail.com>
2019-08-09 14:29:58 +0200

    Added profiles for map and Data JPA implementation

--- pet-clinic-data/src/main/java/atoll/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/atoll/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -2,11 +2,13 @@ package atoll.springframework.sfgpetclinic.services.map;
 
 import atoll.springframework.sfgpetclinic.model.Pet;
 import atoll.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
